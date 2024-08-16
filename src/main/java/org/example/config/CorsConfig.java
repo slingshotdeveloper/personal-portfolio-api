72ae981be930f48ab995e4fc9760df17682feb82
davisdavid <david_davis1@alumni.baylor.edu>
2024-08-16 17:55:01 -0500

    test

--- src/main/java/org/example/config/CorsConfig.java
+++ src/main/java/org/example/config/CorsConfig.java
@@ -1,7 +1,7 @@
 package org.example.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
