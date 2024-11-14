cbe9cc36bf3c5829b3ac7aa8eeab493a90a6e471
slingshotdeveloper <slingshotdevelop@gmail.com>
2024-11-14 14:38:49 -0600

    update email service

--- src/main/java/org/example/service/EmailService.java
+++ src/main/java/org/example/service/EmailService.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.InternetAddress;
+import java.io.UnsupportedEncodingException;
 
 @Service
 public class EmailService {
@@ -14,14 +18,20 @@ public class EmailService {
     private JavaMailSender mailSender;
     @Value("${email.address}")
     private String myEmail;
-    public void sendEmail(ContactForm contactForm) throws MessagingException {
+    @Value("${display.name}")
+    private String displayName;
+    public void sendEmail(ContactForm contactForm) throws MessagingException, UnsupportedEncodingException {
         try {
             String firstName = capitalizeFirstLetter(contactForm.getFirstName());
             String lastName = capitalizeFirstLetter(contactForm.getLastName());
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setTo(myEmail);
-            message.setSubject("Dev Inquiry - " + contactForm.getSubject());
-            message.setText("From: " + firstName + " " + lastName + "\n\n" + "Email: " + contactForm.getEmail() + "\n\n" + "Message: " + "\n" + contactForm.getMessage());
+            String fullName = firstName + " " + lastName;
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message);
+
+            helper.setFrom(new InternetAddress(myEmail, fullName));
+            helper.setTo(myEmail);
+            helper.setSubject("Dev Inquiry - " + contactForm.getSubject());
+            helper.setText("From: " + firstName + " " + lastName + "\n\n" + "Email: " + contactForm.getEmail() + "\n\n" + "Message: " + "\n" + contactForm.getMessage());
             mailSender.send(message);
 
             sendConfirmationEmail(contactForm.getEmail(), firstName);
@@ -34,10 +44,14 @@ public class EmailService {
 
     private void sendConfirmationEmail(String recipientEmail, String firstName) throws Exception {
         try {
-            SimpleMailMessage confirmationMessage = new SimpleMailMessage();
-            confirmationMessage.setTo(recipientEmail);
-            confirmationMessage.setSubject("Message Confirmation");
-            confirmationMessage.setText("Hey " + firstName +",\n\nThanks for reaching out! I have received your message and will get back to you shortly.\n\nBlessings,\n\nDavid Davis");
+            MimeMessage confirmationMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(confirmationMessage);
+            String fromEmail = myEmail;
+            helper.setFrom(new InternetAddress(fromEmail, displayName));
+            helper.setTo(recipientEmail);
+            helper.setSubject("Message Confirmation");
+            helper.setText("Hey " + firstName + ",\n\nThanks for reaching out! I have received your message and will get back to you shortly.\n\nBlessings,\n\nDavid Davis");
+
             mailSender.send(confirmationMessage);
         } catch (Exception e) {
             throw new Exception("Failed to send email: " + e.getMessage(), e);
